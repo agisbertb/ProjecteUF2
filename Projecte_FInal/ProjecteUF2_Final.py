99dc9cfbc22f6151a217d6d9494f11468614d75b
omarelbalaoui <omarelbalaoui@iesebre.com>
2023-03-14 13:18:10 +0100

    Ordenat/Falta arreglar alguns problemes

--- Projecte_FInal/ProjecteUF2_Final.py
+++ Projecte_FInal/ProjecteUF2_Final.py
@@ -41,17 +41,15 @@ def reiniciar():
       vides = 6
       marcador_partides.config(text="Partides: "+str(partides)) 
 
-      #Borrar contenido de guardades
+      #Borrar contingut de guardades
       guardades = set()
-      #Borrar contenido de eguions
+      #Borrar contingut de eguions
       eguions.config(text="")
-      #Borrar contenido de eresultat
-      eresultat.config(text="")
-      #Borrar contenido de marcador_vides
+      #Borrar contingut de marcador_vides
       marcador_vides.config(text="Vides: "+str(vides))
-      #Borrar contenido de marcador_lletra
-      marcador_lletra.config(text="Lletra adivinades: "+str(guardades))
-      #Borrar contenido de entrada
+      #Borrar contingut de marcador_lletra
+      marcador_lletra.config(text="Lletres adivinades: "+str(guardades))
+      #Borrar contingut de entrada
       entrada.delete(0, END)
       l2.config(image=img_inici)
       aleatori()
@@ -70,29 +68,21 @@ def comprovar():
                         if paraula[i] == lletra:
                                     eguions.config(text=eguions.cget("text")[:i] + lletra + eguions.cget("text")[i+1:])
                   if "-" not in eguions.cget("text"):
-                        eresultat.config(text="Has guanyat!")
                         l2.config(image=imgG)
-                  else:
-                        eresultat.config(text="Correcte")
       else:
                   vides -= 1
                   if vides == 0:
-                        eresultat.config(text="Has perdut!")
+                        l2.config(image=imgP)
                   else:
-                        eresultat.config(text="Incorrecte")
                         forca()
-                        eresultat.config(text="")
       marcador_vides.config(text="Vides: "+str(vides))              
-      marcador_lletra.config(text="Lletra adivinades: "+str(guardades))
+      marcador_lletra.config(text="Lletres adivinades: "+str(guardades))
       entrada.delete(0, END)
 
 win = Tk()
-win.geometry("640x400")
-win.configure(bg = 'yellow')
+win.geometry("700x400")
+win.configure(bg = 'Orange')
 win.title('PokePenjat')
-
-l1 = Label(text="Aplicació PenjAPP", background="black", foreground="white", width=80, border= 10, anchor="center")
-l1.grid(column=0,row=0,columnspan=2)
 
 defaultimg = Image.open("imatges/Inici.png")
 defaultimg = defaultimg.resize((300, 200))
@@ -103,37 +93,47 @@ img_inici = img_inici.resize((200, 200))
 img_inici = ImageTk.PhotoImage(img_inici)
 
 imgG = Image.open("imatges/Guanyar.png")
-imgG = imgG.resize((200, 100))
+imgG = imgG.resize((300, 200))
 imgG = ImageTk.PhotoImage(imgG)
 
+imgP = Image.open("imatges/Perdre.png")
+imgP = imgP.resize((300, 200))
+imgP = ImageTk.PhotoImage(imgP)
+
+imgPK = Image.open("imatges/logoPK.png")
+imgPK = imgPK.resize((200, 200))
+imgPK = ImageTk.PhotoImage(imgPK)
+
+l1 = Label(text="Aplicació PenjAPP", background="black", foreground="white", width=90, border= 10, anchor="center")
+l1.place(x=0,y=0)
+
 l2 = Label(image=defaultimg)
-l2.grid(column=0,row=1,columnspan=2)
+l2.place(x=50,y=50)
+
+l3 = Label(image=imgPK)
+l3.place(x=400,y=150)
 
 eguions = Label(text="Paraula")
-eguions.grid(column=0,row=2,columnspan=2)
-
-eresultat = Label(text="Resultat")
-eresultat.grid(column=0,row=6,columnspan=2)
-
+eguions.place(x=170,y=255)
 
 entrada = Entry()
-entrada.grid(column=0,row=3,columnspan=2)
+entrada.place(x=120,y=280)
 
-marcador_vides = Label(text="Vides: "+str(vides))
-marcador_vides.grid(column=0,row=7,sticky=E)
+marcador_vides = Label(text="Vides: "+str(vides), bg="Orange")
+marcador_vides.place(x=400,y=50)
 
-marcador_lletra = Label(text="Lletres adivinades: ")
-marcador_lletra.grid(column=0,row=8,sticky=W)
+marcador_lletra = Label(text="Lletres adivinades: ", bg="Orange", wraplength=200)
+marcador_lletra.place(x=400,y=90)
 
-marcador_partides = Label(text="Partides jugades: ")
-marcador_partides.grid(column=0,row=9,sticky=W)
+marcador_partides = Label(text="Partides jugades: ", bg="Orange")
+marcador_partides.place(x=400,y=70)
 
 #Botons
 bcomençar = Button(text="Començar", command=començar)
-bcomençar.grid(column=0,row=4,sticky=E)
+bcomençar.place(x=135,y=310)
 bsortir = Button(text="Sortir", command=win.destroy)
-bsortir.grid(column=0,row=4,sticky=W)
+bsortir.place(x=250,y=310)
 bjugar = Button(text="Jugar", command=comprovar)
-bjugar.grid(column=0,row=5,sticky=W)
+bjugar.place(x=50,y=310)
 
 win.mainloop()
